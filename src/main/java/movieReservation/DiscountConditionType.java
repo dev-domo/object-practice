205578e665c6363cf442d7fe22ead7ba5d89eb2e
pakaheo <k70923@konkuk.ac.kr>
2024-11-15 16:24:50 +0900

    refactor : 할인 조건 타입 및 영화 타입 삭제 (인터페이스로 구현)

--- src/main/java/movieReservation/DiscountConditionType.java
+++
@@ -1,6 +0,0 @@
-package movieReservation;
-
-public enum DiscountConditionType {
-    PERIOD,
-    SEQUENCE;
-}
--- src/main/java/movieReservation/MovieType.java
+++
@@ -1,8 +0,0 @@
-package movieReservation;
-
-public enum MovieType {
-
-    AMOUNT_DISCOUNT,
-    PERCENT_DISCOUNT,
-    NONE_DISCOUNT
-}
